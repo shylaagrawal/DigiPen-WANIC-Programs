d0dd2551a6aee9ea3be9d286c9faf61ac175c93d
Shyla Agrawal <shylaagrawal@gmail.com>
2025-07-20 14:49:17 -0700

    Add files via upload

---
+++ 12_months_of_the_year_2025_07_18_21_49_00/sketch.js
@@ -0,0 +1,14 @@
+//--------------------------------------------------------
+// Shyla Agrawal
+// 6-30-24
+// This program will tell you all the months of the year using an array
+//--------------------------------------------------------
+
+function setup() {
+  let months =  ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+  
+  for(let i = 0; i < months.length; ++i) {
+    console.log(months[i])
+  }
+
+}
